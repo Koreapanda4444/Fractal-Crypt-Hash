fa73689cfa406788fc9e64dd4375166f0431b5c8
koreapanda4444 <jun20091019@gmail.com>
2026-01-09 08:43:20 +0900

    feat(combine): implement tree merge and recompression

--- src/leaf.c
+++ src/leaf.c
@@ -1,11 +1,46 @@
+#include <string.h>
+#include <stdlib.h>
+
 #include "leaf.h"
+#include "params.h"
+#include "sbox.h"
+
+static inline uint64_t rotl64(uint64_t x, int r) {
+    return (x << r) | (x >> (64 - r));
+}
 
 void fch_leaf_compress(
     const uint8_t *data,
     size_t length,
     fch_state_t *out
 ) {
-    (void)data;
-    (void)length;
-    (void)out;
+    size_t S = out->words;
+    uint64_t *state = out->state;
+
+    for (size_t i = 0; i < S; i++) {
+        state[i] = 0x9E3779B97F4A7C15ULL ^ (uint64_t)i;
+    }
+
+    for (size_t i = 0; i < length; i++) {
+        size_t idx = i % S;
+        uint64_t v = (uint64_t)data[i];
+
+        state[idx] ^= v;
+        state[idx] += rotl64(state[(idx + 1) % S], (idx + 3) * 7);
+        state[(idx + 2) % S] ^= rotl64(state[idx], (idx + 5) * 11);
+    }
+
+    for (size_t i = 0; i < S; i++) {
+        uint8_t *b = (uint8_t *)&state[i];
+        for (int j = 0; j < 8; j++) {
+            b[j] = FCH_SBOX[b[j]];
+        }
+    }
+
+    size_t half = S / 2;
+    for (size_t i = 0; i < half; i++) {
+        state[i] ^= state[i + half];
+    }
+
+    out->words = half;
 }
